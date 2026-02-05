55b6887ef9234d499e9dcd7b44607a000f662127
TheVugx <thevugx@gmail.com>
2026-02-05 14:04:35 -0300

    feature/animacion flip mago

--- Scripts/mago.gd
+++ Scripts/mago.gd
@@ -1,1 +1,23 @@
 extends CharacterBody2D
+
+@onready var sprite := $Sprite2D
+@onready var timer := $Timer
+var direction = 1
+
+func _ready():
+	timer.wait_time = 0.6
+	timer.start()
+
+func _physics_process(delta):
+	if direction == 1:
+		sprite.flip_h = false
+	elif direction == -1:
+		sprite.flip_h = true
+
+func _on_timer_timeout():
+	if randf() < 0.3:
+		if direction == 1:
+			direction = -1
+		elif direction == -1:
+			direction = 1
+	timer.start()
